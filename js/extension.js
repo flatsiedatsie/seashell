7c06e3c85e9328fb21e6a567fa2fa48a5093877f
flatsiedatsie <unfold@gmail.com>
2020-12-08 14:23:07 +0100

    1.0.0
    webthings 1.0
    automation

--- js/extension.js
+++ js/extension.js
@@ -9,6 +9,9 @@
         .then((res) => res.text())
         .then((text) => {
           this.content = text;
+		  if( document.location.href.endsWith("seashell") ){
+		  	this.show();
+		  }
         })
         .catch((e) => console.error('Failed to fetch content:', e));
     }
